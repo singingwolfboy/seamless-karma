b14049b2a13339453ee4f8adf1fb250beb8a0b6a
David Baumgold <david@davidbaumgold.com>
2014-01-25 08:19:51 -0500

    Updated requirements files

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
-import os
 import sys
+
 
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         # import here, because outside the eggs aren't loaded
         import pytest
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
-with open('requirements.txt') as f:
-    required = [req for req in f.read().splitlines()
-                if req and not req.startswith("git+https")]
+
+def extract_requirements(f):
+    return [
+        req for req in f.read().splitlines()
+        if not req.startswith("-r") and not req.startswith("#")
+        and not req.startswith("git+")
+    ]
+
 
 setup(
     name="seamless_karma",
@@ -27,8 +33,8 @@ setup(
     url="http://www.seamlesskarma.com",
     description="track and manage your Seamless.com corporate allocations",
     license="MIT",
-    install_requires=required,
-    tests_require=['pytest', 'factory_boy'],
+    install_requires=extract_requirements(open('requirements.txt')),
+    tests_require=extract_requirements(open('dev-requirements.txt')),
     cmdclass={'test': PyTest},
     zip_safe=False,
 )
