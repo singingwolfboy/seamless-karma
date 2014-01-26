36336456c6331f11bbe5b31d75a95d597b44d5ac
David Baumgold <david@davidbaumgold.com>
2014-01-26 08:59:57 -0500

    set SQLALCHEMY_DATABASE_URI in config files

--- seamless_karma/config/dev.py
+++ seamless_karma/config/dev.py
@@ -1,4 +1,7 @@
 # development configuration
+import os
+
 DEBUG = True
 SECRET_KEY = "dummy"
 CACHE_NO_NULL_WARNING = True
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///seamless_karma.db")
--- seamless_karma/config/prod.py
+++ seamless_karma/config/prod.py
@@ -1,6 +1,6 @@
 # production configuration
-
 import os
 
 DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY", '\x1c\x19\x90\xaf\x1c\x03(\xbc\n\xf03\x9e\x08,\xafgO\xf0\xb7\xaar\x8b\xc5\x9d')
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgres://localhost/seamless_karma")
