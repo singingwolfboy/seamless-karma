d84350d0b155c562510c572eecabeef1bd486f8b
David Baumgold <david@davidbaumgold.com>
2014-01-26 10:22:42 -0500

    added doc for vendor APIs

--- seamless_karma/restful/vendor.py
+++ seamless_karma/restful/vendor.py
@@ -27,9 +27,71 @@ class VendorList(Resource):
 
     @resource_list(Vendor, mfields, parser=make_optional(parser))
     def get(self):
+        """
+        Return a list of all vendors.
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+
+            {
+              "count": 3,
+              "data": [
+                {
+                  "id": 1,
+                  "seamless_id": 2345,
+                  "name": "China Palace",
+                  "latitude": null,
+                  "longitude": null
+                }, {
+                  "id": 2,
+                  "seamless_id": 569452,
+                  "name": "Ponzu",
+                  "latitude": 14.003424,
+                  "longitude": 284.342345
+                }, {
+                  "id": 3,
+                  "seamless_id": null,
+                  "name": "Bob's Steakhouse",
+                  "latitude": -83.230234,
+                  "longitude": 320.432534
+                }
+              ]
+            }
+        """
         return Vendor.query
 
     def post(self):
+        """
+        Create a new vendor (restaurant).
+
+        :form name: *Required* The name of the vendor on Seamless_
+        :form seamless_id: *Optional* The ID of the vendor on Seamless_
+        :form latitude: *Optional* The latitude corresponding to the vendor's
+            location
+        :form longitude: *Optional* The longitude corresponding to the vendor's
+            location
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 201 CREATED
+            Content-Type: application/json
+            Location: /api/vendors/7
+
+            {
+              "message": "created",
+              "id": 7
+            }
+
+        :status 201: the vendor was successfully created
+
+        .. _Seamless: http://www.seamless.com
+        """
         args = parser.parse_args()
         vendor = Vendor(**args)
         db.session.add(vendor)
@@ -50,10 +112,67 @@ class VendorDetail(Resource):
 
     @marshal_with(mfields)
     def get(self, vendor_id):
+        """
+        Return information about a specific vendor, identified by ID.
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+
+            {
+              "id": 93,
+              "seamless_id": 230934,
+              "name": "Bob's Diner",
+              "latitude": 83.23834,
+              "longitude": -23.340924
+            }
+
+        :status 200: no error
+        :status 404: there is no vendor with the given ID
+        """
         return self.get_vendor_or_abort(vendor_id)
 
     @marshal_with(mfields)
     def put(self, vendor_id):
+        """
+        Update information about a specific vendor, identified by ID.
+
+        Example request:
+
+        .. sourcecode:: http
+
+            PUT /api/vendors/12 HTTP/1.1
+            Host: seamlesskarma.com
+            Content-Type: application/x-www-form-urlencoded
+            Content-Length: 17
+
+            seamless_id=9
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+
+            {
+              "id": 12,
+              "seamless_id": 9,
+              "name": "Bob's Wings",
+              "latitude": null,
+              "longitude": null
+            }
+
+        :form seamless_id: *Optional* updated Seamless ID
+        :form name: *Optional* updated name
+        :form latitude: *Optional* updated latitude
+        :form longitude: *Optional* updated longitude
+        :status 200: the vendor was updated
+        :status 404: there is no vendor with the given ID
+        """
         vendor = self.get_vendor_or_abort(vendor_id)
         args = make_optional(parser).parse_args()
         for attr in ('seamless_id', 'name', 'latitude', 'longitude'):
@@ -64,6 +183,23 @@ class VendorDetail(Resource):
         return vendor
 
     def delete(self, vendor_id):
+        """
+        Delete a specific vendor, identified by ID.
+
+        Example response:
+
+        .. sourcecode:: http
+
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+
+            {
+              "message": "deleted"
+            }
+
+        :status 200: the vendor was deleted
+        :status 404: there is no vendor with the given ID
+        """
         vendor = self.get_vendor_or_abort(vendor_id)
         db.session.delete(vendor)
         db.session.commit()
