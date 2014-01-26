7425245e94c46003192f4d17dab49f258b94539b
David Baumgold <david@davidbaumgold.com>
2014-01-26 09:06:58 -0500

    Moved Currency type to a separete file

--- seamless_karma/models.py
+++ seamless_karma/models.py
@@ -1,58 +1,10 @@
 from seamless_karma.extensions import db
+from seamless_karma.sql_types import Currency
 import sqlalchemy as sa
 from sqlalchemy.orm import backref
-from sqlalchemy.sql import type_api, sqltypes, type_coerce
+from sqlalchemy.sql import type_coerce
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 from decimal import Decimal
-
-
-class Currency(type_api.TypeDecorator):
-    """
-    A SQLAlchemy type that accurately saves Decimal values to a fixed number
-    of places (2 by default). This is similar to SQLAlchemy's builtin
-    Numeric type, but differs in its fallback implementation: Numeric falls
-    back on saving as floats, while Currency falls back on saving as integers,
-    making it more suitable for database computation (such as SUM, AVG, and
-    other SQL functions).
-
-    This type has only been tested to work properly on postgresql (built-in
-    numeric support) and sqlite (fallback integer support).
-    """
-    impl = type_api.TypeEngine
-
-    def __init__(self, precision=None, scale=2, *args, **kwargs):
-        self.precision = precision
-        self.scale = scale
-        self.quantize = Decimal("10") ** -self.scale
-        super(Currency, self).__init__(*args, **kwargs)
-
-    def asdecimal(self, dialect):
-        return dialect.supports_native_decimal or sqltypes.Numeric in dialect.colspecs
-
-    def load_dialect_impl(self, dialect):
-        if self.asdecimal(dialect):
-            return dialect.type_descriptor(sqltypes.Numeric)
-        else:
-            return dialect.type_descriptor(sqltypes.Integer)
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        if self.asdecimal(dialect):
-            return float(value)
-        else:
-            # store as an integer
-            return int(value / self.quantize)
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        d = Decimal(value)
-        if self.asdecimal(dialect):
-            return d.quantize(self.quantize)
-        else:
-            # rescale our stored integer
-            return d * self.quantize
 
 
 class Organization(db.Model):
---
+++ seamless_karma/sql_types.py
@@ -0,0 +1,51 @@
+from decimal import Decimal
+from sqlalchemy.sql import type_api, sqltypes
+
+
+class Currency(type_api.TypeDecorator):
+    """
+    A SQLAlchemy type that accurately saves Decimal values to a fixed number
+    of places (2 by default). This is similar to SQLAlchemy's builtin
+    Numeric type, but differs in its fallback implementation: Numeric falls
+    back on saving as floats, while Currency falls back on saving as integers,
+    making it more suitable for database computation (such as SUM, AVG, and
+    other SQL functions).
+
+    This type has only been tested to work properly on postgresql (built-in
+    numeric support) and sqlite (fallback integer support).
+    """
+    impl = type_api.TypeEngine
+
+    def __init__(self, precision=None, scale=2, *args, **kwargs):
+        self.precision = precision
+        self.scale = scale
+        self.quantize = Decimal("10") ** -self.scale
+        super(Currency, self).__init__(*args, **kwargs)
+
+    def asdecimal(self, dialect):
+        return dialect.supports_native_decimal or sqltypes.Numeric in dialect.colspecs
+
+    def load_dialect_impl(self, dialect):
+        if self.asdecimal(dialect):
+            return dialect.type_descriptor(sqltypes.Numeric)
+        else:
+            return dialect.type_descriptor(sqltypes.Integer)
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        if self.asdecimal(dialect):
+            return float(value)
+        else:
+            # store as an integer
+            return int(value / self.quantize)
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        d = Decimal(value)
+        if self.asdecimal(dialect):
+            return d.quantize(self.quantize)
+        else:
+            # rescale our stored integer
+            return d * self.quantize
